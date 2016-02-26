da3cb201cd16bc88b583baecee30b5a8cca015c5
evan <evan@blackbox.localdomain>
2016-02-26 15:21:57 -0500

    copy images when building

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 #Evan Widloski - 2015-01-17
 #Search for markdown files and build them
 
-find `pwd` -name "*.md" -exec "find_call" {} \;
+find . -regex ".*\(org\|png\|jpg\)" -not -path "*ltxpng*" -exec "find_call" {} \;
