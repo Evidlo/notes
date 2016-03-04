6d80a6a521a741b5c19a8f445ab19dee2f926007
evan <evan@evanw.org>
2016-03-04 11:18:28 -0500

    added root locus construction notes, simplified directory structure

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 #Evan Widloski - 2015-01-17
 #Search for markdown files and build them
 
-find ./content -regex ".*\(org\|png\|jpg\|css\|js\|ttf\)" -not -path "*ltxpng*" -exec "find_call" {} \;
+cd content
+find . -regex ".*\(org\|png\|jpg\|css\|js\|ttf\)" -not -path "*ltxpng*" -exec "../find_call" {} \;
